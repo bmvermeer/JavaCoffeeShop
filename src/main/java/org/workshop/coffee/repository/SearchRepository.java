fc040d35bcc51ff725c64e5383611e92c331f0cb
Liran Tal <liran.tal@gmail.com>
2023-11-07 10:56:59 +0200

    feat: create a search repository functionality

--- src/main/java/org/workshop/coffee/repository/SearchRepository.java
+++ src/main/java/org/workshop/coffee/repository/SearchRepository.java
@@ -19,7 +19,19 @@ public class SearchRepository {
     DataSource dataSource;
 
     public List<Product> searchProduct (String input) {
-        return null;
+        var lowerInput = input.toLowerCase(Locale.ROOT);
+
+        // create a query matching input to description and product_name
+        var query = em.createNativeQuery("Select * from Product where lower(description) like '%" + lowerInput + "%' OR lower(product_name) like '%" + lowerInput + "%'", Product.class);
+        // instead of the above dangerous query, use a named parameter input to description and product_name
+        // var query = em.createNativeQuery("Select * from Product where lower(description) like :input OR lower(product_name) like :input", Product.class);
+
+        // set the named parameter input to the value of lowerInput
+        // query.setParameter("input", "%" + lowerInput + "%");
+
+        // execute the query and return the result
+        var resultList = (List<Product>) query.getResultList();
+        return resultList;
     }
 
 }
